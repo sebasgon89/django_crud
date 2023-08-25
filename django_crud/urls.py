dc66500ead1843344f849536a12754e91fbb1ba3
sebasgon89 <seba3789@gmail.com>
2023-08-25 16:37:57 -0300

    Added user creation validations

--- django_crud/urls.py
+++ django_crud/urls.py
@@ -21,5 +21,6 @@ from task import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('signup/', views.signup)
+    path('', views.home, name="home"),
+    path('signup/', views.signup, name="signup")
 ]
--- task/views.py
+++ task/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
 
 def home(request):
     return render(request, "home.html")
 
 
 def signup(request):
-    return render(request, "signup.html",{
-        'form':UserCreationForm
+    if request.method == "GET":
+        return render(request, "signup.html", {
+            'form': UserCreationForm
+        })
+    else:
+        try:
+            if request.POST['password1'] == request.POST['password2']:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse("User created successfully")
+        except:
+            return render(request, "signup.html", {
+                'form': UserCreationForm,
+                'error': 'User already exists'
+            })
+    return render(request, "signup.html", {
+        'form': UserCreationForm,
+        'error': 'Passwords do not match'
     })
-
