d7fe46fc974e922fb74ad28abd9f730e9e393c5c
sebasgon89 <seba3789@gmail.com>
2023-08-29 11:34:18 -0300

    Added start of login. 50:00

--- django_crud/urls.py
+++ django_crud/urls.py
@@ -21,6 +21,7 @@ from task import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('home/', views.home, name="home"),
     path('signup/', views.signup, name="signup"),
     path('tasks/', views.tasks, name="tasks"),
 ]
