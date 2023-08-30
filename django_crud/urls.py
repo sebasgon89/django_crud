1d1c287d5f2df818dcb87ab73fa57aa0125a0dab
sebasgon89 <seba3789@gmail.com>
2023-08-30 16:13:20 -0300

    Start of create task. 1:29

--- django_crud/urls.py
+++ django_crud/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('home/', views.home, name="home"),
     path('signup/', views.signup, name="signup"),
     path('tasks/', views.tasks, name="tasks"),
+    path('tasks/create/', views.create_task, name="create_tasks"),
     path('logout/', views.signout, name="logout"),
     path('signin/',views.signin, name="signin")
 ]
--- task/views.py
+++ task/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth import login, logout,authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
 
 
 def home(request):
@@ -36,6 +37,17 @@ def signup(request):
 
 def tasks(request):
     return render(request, "tasks.html")
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, "create_task.html",{
+            'form':TaskForm
+        })
+    else:
+        print(request.POST)
+        return render(request, "create_task.html",{
+            'form':TaskForm
+        })
 
 
 def signout(request):
