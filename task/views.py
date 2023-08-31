0d1b4fce4ccd3437e425bc3b3ad151dbd530939c
sebasgon89 <seba3789@gmail.com>
2023-08-31 12:02:18 -0300

    Added Tasks list

--- task/views.py
+++ task/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, logout,authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
+from .models import Task
 
 
 def home(request):
@@ -36,7 +37,11 @@ def signup(request):
 
 
 def tasks(request):
-    return render(request, "tasks.html")
+    tasks = Task.objects.filter(user=request.user)
+
+    return render(request, "tasks.html",{
+        'tasks': tasks
+    })
 
 def create_task(request):
     if request.method == "GET":
