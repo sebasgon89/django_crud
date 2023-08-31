6bfdd3cd349d3bbe794ed90d2a23aa9a378c4930
sebasgon89 <seba3789@gmail.com>
2023-08-31 11:41:59 -0300

    Added create task

--- task/views.py
+++ task/views.py
@@ -44,9 +44,16 @@ def create_task(request):
             'form':TaskForm
         })
     else:
-        print(request.POST)
-        return render(request, "create_task.html",{
-            'form':TaskForm
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect("tasks")
+        except ValueError:
+            return render(request, "create_task.html",{
+            'form':TaskForm,
+            'error':"Please provide vaid data"
         })
 
 
